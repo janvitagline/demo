a25967f00406363e239488c5c072e47225d49e58
mac <mac@macs-iMac-2.local>
2022-02-01 15:59:20 +0530

    api add

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/abc", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
